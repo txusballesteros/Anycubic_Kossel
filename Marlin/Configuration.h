7c9f4b2892145c02e0bcf3a87196507eff8534c0
Txus Ballesteros <txus.ballesteros@gmail.com>
2020-04-09 11:44:49 +0200

    Update Drivers Definition to TMC2208_STANDALONE

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -771,7 +771,7 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 5.0
+#define DEFAULT_XJERK                 10.0
 #define DEFAULT_YJERK                 DEFAULT_XJERK
 #define DEFAULT_ZJERK                 DEFAULT_XJERK // Must be same as XY for delta
 #define DEFAULT_EJERK                 DEFAULT_XJERK
