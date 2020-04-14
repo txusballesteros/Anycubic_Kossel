e5da19fdefb5165466e50ead52a769dafb6f77dc
Txus Ballesteros <txus.ballesteros@gmail.com>
2020-04-14 10:25:55 +0200

    Update Hotend PID Values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -409,9 +409,9 @@
   //#define DEFAULT_Kd 440
 
   // Anycubic Kossel - run 'M106 S255' & 'M303 E0 C10 S200'
-  #define DEFAULT_Kp 22.36
-  #define DEFAULT_Ki 1.63
-  #define DEFAULT_Kd 76.48
+  #define DEFAULT_Kp 15.77
+  #define DEFAULT_Ki 0.93
+  #define DEFAULT_Kd 66.84
 
 #endif // PIDTEMP
 
